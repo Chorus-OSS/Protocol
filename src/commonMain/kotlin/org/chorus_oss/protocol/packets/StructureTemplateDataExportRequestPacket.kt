f4362c243821101d4cb478208737f3fed7739641
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-21 21:26:29 +1200

    structure template data packets

---
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/StructureTemplateDataExportRequestPacket.kt
@@ -0,0 +1,66 @@
+package org.chorus_oss.protocol.packets
+
+import kotlinx.io.Sink
+import kotlinx.io.Source
+import org.chorus_oss.protocol.ProtocolInfo
+import org.chorus_oss.protocol.core.Packet
+import org.chorus_oss.protocol.core.PacketCodec
+import org.chorus_oss.protocol.core.Proto
+import org.chorus_oss.protocol.core.ProtoCodec
+import org.chorus_oss.protocol.core.types.Byte
+import org.chorus_oss.protocol.core.types.String
+import org.chorus_oss.protocol.types.IVector3
+import org.chorus_oss.protocol.types.UIVector3
+import org.chorus_oss.protocol.types.structure.StructureSettings
+
+data class StructureTemplateDataExportRequestPacket(
+    val structureName: String,
+    val position: IVector3,
+    val settings: StructureSettings,
+    val requestType: RequestType,
+) : Packet(id) {
+    companion object : PacketCodec<StructureTemplateDataExportRequestPacket> {
+        enum class RequestType(val net: Byte) {
+            ExportFromSave(1),
+            ExportFromLoad(2),
+            QuerySavedStructure(3);
+
+            companion object : ProtoCodec<RequestType> {
+                override fun serialize(
+                    value: RequestType,
+                    stream: Sink
+                ) {
+                    Proto.Byte.serialize(value.net, stream)
+                }
+
+                override fun deserialize(stream: Source): RequestType {
+                    return Proto.Byte.deserialize(stream).let {
+                        entries.find { e -> e.net == it }!!
+                    }
+                }
+            }
+        }
+
+        override val id: Int
+            get() = ProtocolInfo.STRUCTURE_TEMPLATE_DATA_EXPORT_REQUEST_PACKET
+
+        override fun serialize(
+            value: StructureTemplateDataExportRequestPacket,
+            stream: Sink
+        ) {
+            Proto.String.serialize(value.structureName, stream)
+            UIVector3.serialize(value.position, stream)
+            StructureSettings.serialize(value.settings, stream)
+            RequestType.serialize(value.requestType, stream)
+        }
+
+        override fun deserialize(stream: Source): StructureTemplateDataExportRequestPacket {
+            return StructureTemplateDataExportRequestPacket(
+                structureName = Proto.String.deserialize(stream),
+                position = UIVector3.deserialize(stream),
+                settings = StructureSettings.deserialize(stream),
+                requestType = RequestType.deserialize(stream),
+            )
+        }
+    }
+}
---
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/StructureTemplateDataExportResponsePacket.kt
@@ -0,0 +1,73 @@
+package org.chorus_oss.protocol.packets
+
+import kotlinx.io.Sink
+import kotlinx.io.Source
+import org.chorus_oss.nbt.Tag
+import org.chorus_oss.nbt.TagSerialization
+import org.chorus_oss.nbt.tags.CompoundTag
+import org.chorus_oss.protocol.ProtocolInfo
+import org.chorus_oss.protocol.core.Packet
+import org.chorus_oss.protocol.core.PacketCodec
+import org.chorus_oss.protocol.core.Proto
+import org.chorus_oss.protocol.core.ProtoCodec
+import org.chorus_oss.protocol.core.types.Boolean
+import org.chorus_oss.protocol.core.types.Byte
+import org.chorus_oss.protocol.core.types.String
+
+data class StructureTemplateDataExportResponsePacket(
+    val structureName: String,
+    val success: Boolean,
+    val structureTemplate: CompoundTag?,
+    val responseType: ResponseType,
+) : Packet(id) {
+    companion object : PacketCodec<StructureTemplateDataExportResponsePacket> {
+        enum class ResponseType(val net: Byte) {
+            Export(1),
+            Query(2);
+
+            companion object : ProtoCodec<ResponseType> {
+                override fun serialize(
+                    value: ResponseType,
+                    stream: Sink
+                ) {
+                    Proto.Byte.serialize(value.net, stream)
+                }
+
+                override fun deserialize(stream: Source): ResponseType {
+                    return Proto.Byte.deserialize(stream).let {
+                        entries.find { e -> e.net == it }!!
+                    }
+                }
+            }
+        }
+
+        override val id: Int
+            get() = ProtocolInfo.STRUCTURE_TEMPLATE_DATA_EXPORT_RESPONSE_PACKET
+
+        override fun serialize(
+            value: StructureTemplateDataExportResponsePacket,
+            stream: Sink
+        ) {
+            Proto.String.serialize(value.structureName, stream)
+            Proto.Boolean.serialize(value.success, stream)
+            when (value.success) {
+                true -> Tag.serialize(value.structureTemplate as CompoundTag, stream, TagSerialization.NetLE, true)
+                false -> Unit
+            }
+            ResponseType.serialize(value.responseType, stream)
+        }
+
+        override fun deserialize(stream: Source): StructureTemplateDataExportResponsePacket {
+            val success: Boolean
+            return StructureTemplateDataExportResponsePacket(
+                structureName = Proto.String.deserialize(stream),
+                success = Proto.Boolean.deserialize(stream).also { success = it },
+                structureTemplate = when (success) {
+                    true -> Tag.deserialize(stream, TagSerialization.NetLE) as CompoundTag
+                    false -> null
+                },
+                responseType = ResponseType.deserialize(stream),
+            )
+        }
+    }
+}
