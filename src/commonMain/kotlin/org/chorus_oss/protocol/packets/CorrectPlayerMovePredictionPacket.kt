535af80849e0b96090ed44b3258f0b96945b8cd6
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-21 22:14:28 +1200

    correct movement prediction packet

---
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/CorrectPlayerMovePredictionPacket.kt
@@ -0,0 +1,89 @@
+package org.chorus_oss.protocol.packets
+
+import kotlinx.io.Sink
+import kotlinx.io.Source
+import org.chorus_oss.protocol.ProtocolInfo
+import org.chorus_oss.protocol.core.Packet
+import org.chorus_oss.protocol.core.PacketCodec
+import org.chorus_oss.protocol.core.Proto
+import org.chorus_oss.protocol.core.ProtoCodec
+import org.chorus_oss.protocol.core.ProtoHelper
+import org.chorus_oss.protocol.core.ProtoLE
+import org.chorus_oss.protocol.core.ProtoVAR
+import org.chorus_oss.protocol.core.types.Boolean
+import org.chorus_oss.protocol.core.types.Byte
+import org.chorus_oss.protocol.core.types.Float
+import org.chorus_oss.protocol.core.types.ULong
+import org.chorus_oss.protocol.types.Vector2f
+import org.chorus_oss.protocol.types.Vector3f
+
+data class CorrectPlayerMovePredictionPacket(
+    val predictionType: PredictionType,
+    val position: Vector3f,
+    val delta: Vector3f,
+    val rotation: Vector2f?,
+    val vehicleAngularVelocity: Float?,
+    val onGround: Boolean,
+    val tick: ULong,
+) : Packet(id) {
+    companion object : PacketCodec<CorrectPlayerMovePredictionPacket> {
+        enum class PredictionType {
+            Player,
+            Vehicle;
+
+            companion object : ProtoCodec<PredictionType> {
+                override fun serialize(
+                    value: PredictionType,
+                    stream: Sink
+                ) {
+                    Proto.Byte.serialize(value.ordinal.toByte(), stream)
+                }
+
+                override fun deserialize(stream: Source): PredictionType {
+                    return entries[Proto.Byte.deserialize(stream).toInt()]
+                }
+            }
+        }
+
+        override val id: Int
+            get() = ProtocolInfo.CORRECT_PLAYER_MOVE_PREDICTION_PACKET
+
+        override fun serialize(
+            value: CorrectPlayerMovePredictionPacket,
+            stream: Sink
+        ) {
+            PredictionType.serialize(value.predictionType, stream)
+            Vector3f.serialize(value.position, stream)
+            Vector3f.serialize(value.delta, stream)
+            when (value.predictionType) {
+                PredictionType.Vehicle -> Vector2f.serialize(value.rotation as Vector2f, stream)
+                else -> Unit
+            }
+            when (value.predictionType) {
+                PredictionType.Vehicle -> ProtoHelper.serializeNullable(value.vehicleAngularVelocity, stream, ProtoLE.Float)
+                else -> Unit
+            }
+            Proto.Boolean.serialize(value.onGround, stream)
+            ProtoVAR.ULong.serialize(value.tick, stream)
+        }
+
+        override fun deserialize(stream: Source): CorrectPlayerMovePredictionPacket {
+            val predictionType: PredictionType
+            return CorrectPlayerMovePredictionPacket(
+                predictionType = PredictionType.deserialize(stream).also { predictionType = it },
+                position = Vector3f.deserialize(stream),
+                delta = Vector3f.deserialize(stream),
+                rotation = when (predictionType) {
+                    PredictionType.Vehicle -> Vector2f.deserialize(stream)
+                    else -> null
+                },
+                vehicleAngularVelocity = when (predictionType) {
+                    PredictionType.Vehicle -> ProtoHelper.deserializeNullable(stream, ProtoLE.Float)
+                    else -> null
+                },
+                onGround = Proto.Boolean.deserialize(stream),
+                tick = ProtoVAR.ULong.deserialize(stream),
+            )
+        }
+    }
+}
