8d01749fdefcc09c2d51f4df4a0f498f14368f6c
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-21 22:57:39 +1200

    award achievement packet

---
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/AwardAchievementPacket.kt
@@ -0,0 +1,28 @@
+package org.chorus_oss.protocol.packets
+
+import kotlinx.io.Sink
+import kotlinx.io.Source
+import org.chorus_oss.protocol.ProtocolInfo
+import org.chorus_oss.protocol.core.Packet
+import org.chorus_oss.protocol.core.PacketCodec
+import org.chorus_oss.protocol.core.ProtoLE
+import org.chorus_oss.protocol.core.types.Int
+
+data class AwardAchievementPacket(
+    val achievementID: Int,
+) : Packet(id) {
+    companion object : PacketCodec<AwardAchievementPacket> {
+        override val id: Int
+            get() = ProtocolInfo.AWARD_ACHIEVEMENT_PACKET
+
+        override fun serialize(value: AwardAchievementPacket, stream: Sink) {
+            ProtoLE.Int.serialize(value.achievementID, stream)
+        }
+
+        override fun deserialize(stream: Source): AwardAchievementPacket {
+            return AwardAchievementPacket(
+                achievementID = ProtoLE.Int.deserialize(stream),
+            )
+        }
+    }
+}
