736715c4c4d8d68f9a21bc058adcfa933a240368
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-21 23:02:37 +1200

    update client options packet

---
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/UpdateClientOptionsPacket.kt
@@ -0,0 +1,53 @@
+package org.chorus_oss.protocol.packets
+
+import kotlinx.io.Sink
+import kotlinx.io.Source
+import org.chorus_oss.protocol.ProtocolInfo
+import org.chorus_oss.protocol.core.Packet
+import org.chorus_oss.protocol.core.PacketCodec
+import org.chorus_oss.protocol.core.Proto
+import org.chorus_oss.protocol.core.ProtoCodec
+import org.chorus_oss.protocol.core.ProtoHelper
+import org.chorus_oss.protocol.core.types.Byte
+
+data class UpdateClientOptionsPacket(
+    val graphicsMode: GraphicsMode?,
+) : Packet(id) {
+    companion object : PacketCodec<UpdateClientOptionsPacket> {
+        enum class GraphicsMode {
+            Simple,
+            Fancy,
+            Advanced,
+            RayTraced;
+
+            companion object : ProtoCodec<GraphicsMode> {
+                override fun serialize(
+                    value: GraphicsMode,
+                    stream: Sink
+                ) {
+                    Proto.Byte.serialize(value.ordinal.toByte(), stream)
+                }
+
+                override fun deserialize(stream: Source): GraphicsMode {
+                    return entries[Proto.Byte.deserialize(stream).toInt()]
+                }
+            }
+        }
+
+        override val id: Int
+            get() = ProtocolInfo.UPDATE_CLIENT_OPTIONS_PACKET
+
+        override fun serialize(
+            value: UpdateClientOptionsPacket,
+            stream: Sink
+        ) {
+            ProtoHelper.serializeNullable(value.graphicsMode, stream, GraphicsMode)
+        }
+
+        override fun deserialize(stream: Source): UpdateClientOptionsPacket {
+            return UpdateClientOptionsPacket(
+                graphicsMode = ProtoHelper.deserializeNullable(stream, GraphicsMode)
+            )
+        }
+    }
+}
