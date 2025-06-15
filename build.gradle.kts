4dc430a745e9a17a40896826133e08acfdfca9b7
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-06-15 23:58:25 +1200

    setup publishing to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("multiplatform") version "2.1.10"
-    `maven-publish`
+    id("org.jetbrains.dokka") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.32.0"
 }
 
 group = "org.chorus_oss"
@@ -28,5 +33,55 @@ kotlin {
                 implementation(libs.chorus.varlen)
             }
         }
+    }
+
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
+
+        coordinates(
+            group.toString(),
+            "protocol",
+            version.toString()
+        )
+
+        pom {
+            name = "Protocol"
+            description = "Minecraft: Bedrock protocol library for Kotlin Multiplatform"
+            inceptionYear = "2025"
+            url = "https://github.com/Chorus-OSS/Protocol"
+            licenses {
+                license {
+                    name = "The Apache License, Version 2.0"
+                    url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                    distribution = "repo"
+                }
+            }
+            developers {
+                developer {
+                    id = "omniacdev"
+                    name = "OmniacDev"
+                    url = "https://github.com/OmniacDev"
+                    email = "omniacdev@chorus-oss.org"
+                }
+            }
+            scm {
+                url = "https://github.com/Chorus-OSS/Protocol"
+                connection = "scm:git:git://github.com/Chorus-OSS/Protocol.git"
+                developerConnection = "scm:git:ssh://github.com/Chorus-OSS/Protocol.git"
+            }
+            issueManagement {
+                system = "GitHub Issues"
+                url = "https://github.com/Chorus-OSS/Protocol/issues"
+            }
+        }
+
+        configure(
+            KotlinMultiplatform(
+                javadocJar = JavadocJar.Dokka("dokkaHtml"),
+                sourcesJar = true,
+                androidVariantsToPublish = emptyList(),
+            )
+        )
     }
 }
