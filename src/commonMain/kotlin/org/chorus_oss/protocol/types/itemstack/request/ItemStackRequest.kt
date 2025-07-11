9f690003a46290ae6021602595f69953df02a4f0
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-12 06:17:48 +1200

    fix ItemStackRequest

--- src/commonMain/kotlin/org/chorus_oss/protocol/types/itemstack/request/ItemStackRequest.kt
+++ src/commonMain/kotlin/org/chorus_oss/protocol/types/itemstack/request/ItemStackRequest.kt
@@ -5,6 +5,7 @@ import kotlinx.io.Source
 import org.chorus_oss.protocol.core.Proto
 import org.chorus_oss.protocol.core.ProtoCodec
 import org.chorus_oss.protocol.core.ProtoHelper
+import org.chorus_oss.protocol.core.ProtoLE
 import org.chorus_oss.protocol.core.ProtoVAR
 import org.chorus_oss.protocol.core.types.Int
 import org.chorus_oss.protocol.core.types.String
@@ -14,7 +15,7 @@ data class ItemStackRequest(
     var requestId: Int,
     var actions: List<ItemStackRequestAction>,
     var filterStrings: List<String>,
-    var textProcessingEventOrigin: FilterCause
+    var textProcessingEventOrigin: FilterCause?
 ) {
     companion object : ProtoCodec<ItemStackRequest> {
         override fun serialize(
@@ -24,7 +25,7 @@ data class ItemStackRequest(
             ProtoVAR.Int.serialize(value.requestId, stream)
             ProtoHelper.serializeList(value.actions, stream, ItemStackRequestAction)
             ProtoHelper.serializeList(value.filterStrings, stream, Proto.String)
-            FilterCause.serialize(value.textProcessingEventOrigin, stream)
+            ProtoLE.Int.serialize(value.textProcessingEventOrigin?.ordinal ?: -1, stream)
         }
 
         override fun deserialize(stream: Source): ItemStackRequest {
@@ -32,7 +33,12 @@ data class ItemStackRequest(
                 requestId = ProtoVAR.Int.deserialize(stream),
                 actions = ProtoHelper.deserializeList(stream, ItemStackRequestAction),
                 filterStrings = ProtoHelper.deserializeList(stream, Proto.String),
-                textProcessingEventOrigin = FilterCause.deserialize(stream)
+                textProcessingEventOrigin = ProtoLE.Int.deserialize(stream).let {
+                    when (it) {
+                        -1 -> null
+                        else -> FilterCause.entries[it]
+                    }
+                }
             )
         }
     }
