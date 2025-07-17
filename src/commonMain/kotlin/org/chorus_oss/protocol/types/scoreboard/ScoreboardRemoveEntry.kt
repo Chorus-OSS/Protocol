93d652dd467b5ac244647c6bbb620b9923ebf719
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-18 00:29:12 +1200

    fix: - ScoreboardEntry - ScoreboardRemoveEntry

--- src/commonMain/kotlin/org/chorus_oss/protocol/types/scoreboard/ScoreboardRemoveEntry.kt
+++ src/commonMain/kotlin/org/chorus_oss/protocol/types/scoreboard/ScoreboardRemoveEntry.kt
@@ -4,6 +4,7 @@ import kotlinx.io.Sink
 import kotlinx.io.Source
 import org.chorus_oss.protocol.core.Proto
 import org.chorus_oss.protocol.core.ProtoCodec
+import org.chorus_oss.protocol.core.ProtoLE
 import org.chorus_oss.protocol.core.ProtoVAR
 import org.chorus_oss.protocol.core.types.Int
 import org.chorus_oss.protocol.core.types.Long
@@ -18,14 +19,14 @@ data class ScoreboardRemoveEntry(
         override fun serialize(value: ScoreboardRemoveEntry, stream: Sink) {
             ProtoVAR.Long.serialize(value.entryID, stream)
             Proto.String.serialize(value.objectiveName, stream)
-            ProtoVAR.Int.serialize(value.score, stream)
+            ProtoLE.Int.serialize(value.score, stream)
         }
 
         override fun deserialize(stream: Source): ScoreboardRemoveEntry {
             return ScoreboardRemoveEntry(
                 entryID = ProtoVAR.Long.deserialize(stream),
                 objectiveName = Proto.String.deserialize(stream),
-                score = ProtoVAR.Int.deserialize(stream),
+                score = ProtoLE.Int.deserialize(stream),
             )
         }
     }
