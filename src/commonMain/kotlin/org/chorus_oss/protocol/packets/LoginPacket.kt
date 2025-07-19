cd8c02a5934809c33aeb57e7e9d56f60bcb8e936
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-19 20:34:36 +1200

    fix LoginPacket: - use ByteString instead of String for connectionRequest

--- src/commonMain/kotlin/org/chorus_oss/protocol/packets/LoginPacket.kt
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/LoginPacket.kt
@@ -2,29 +2,31 @@ package org.chorus_oss.protocol.packets
 
 import kotlinx.io.Sink
 import kotlinx.io.Source
+import kotlinx.io.bytestring.ByteString
 import org.chorus_oss.protocol.core.Packet
 import org.chorus_oss.protocol.core.PacketCodec
 import org.chorus_oss.protocol.core.Proto
 import org.chorus_oss.protocol.core.ProtoBE
+import org.chorus_oss.protocol.core.types.ByteString
 import org.chorus_oss.protocol.core.types.Int
 import org.chorus_oss.protocol.core.types.String
 
 data class LoginPacket(
     val clientProtocol: Int,
-    val connectionRequest: String,
+    val connectionRequest: ByteString,
 ) : Packet(id) {
     companion object : PacketCodec<LoginPacket> {
         override val id: Int = 1
 
         override fun serialize(value: LoginPacket, stream: Sink) {
             ProtoBE.Int.serialize(value.clientProtocol, stream)
-            Proto.String.serialize(value.connectionRequest, stream)
+            Proto.ByteString.serialize(value.connectionRequest, stream)
         }
 
         override fun deserialize(stream: Source): LoginPacket {
             return LoginPacket(
                 clientProtocol = ProtoBE.Int.deserialize(stream),
-                connectionRequest = Proto.String.deserialize(stream),
+                connectionRequest = Proto.ByteString.deserialize(stream),
             )
         }
     }
