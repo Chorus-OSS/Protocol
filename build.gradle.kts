411cc2010f7c6a169aa1ed92c36113af87a248e8
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-07-19 20:36:19 +1200

    bump version: v819.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 description = "Minecraft: Bedrock protocol library for Kotlin Multiplatform"
 group = "org.chorus-oss"
-version = "v819.5"
+version = "v819.6"
 
 repositories {
     mavenCentral()
