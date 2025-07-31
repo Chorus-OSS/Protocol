ddeaadc2156598fb2e14b928566ef3a2b162e3f8
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-08-01 04:25:46 +1200

    format

--- src/commonMain/kotlin/org/chorus_oss/protocol/packets/RemoveVolumeEntityPacket.kt
+++ src/commonMain/kotlin/org/chorus_oss/protocol/packets/RemoveVolumeEntityPacket.kt
@@ -7,7 +7,6 @@ import org.chorus_oss.protocol.core.PacketCodec
 import org.chorus_oss.protocol.core.ProtoVAR
 import org.chorus_oss.protocol.core.types.Int
 import org.chorus_oss.protocol.core.types.UInt
-import org.chorus_oss.protocol.types.ActorRuntimeID
 
 
 data class RemoveVolumeEntityPacket(
