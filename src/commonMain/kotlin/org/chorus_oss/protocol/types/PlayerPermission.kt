4ff17e488759624c77ec53653c191f957e17272a
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-08-01 04:23:14 +1200

    fixes

--- src/commonMain/kotlin/org/chorus_oss/protocol/types/PlayerPermission.kt
+++ src/commonMain/kotlin/org/chorus_oss/protocol/types/PlayerPermission.kt
@@ -4,7 +4,9 @@ import kotlinx.io.Sink
 import kotlinx.io.Source
 import org.chorus_oss.protocol.core.Proto
 import org.chorus_oss.protocol.core.ProtoCodec
+import org.chorus_oss.protocol.core.ProtoVAR
 import org.chorus_oss.protocol.core.types.Byte
+import org.chorus_oss.protocol.core.types.Int
 
 enum class PlayerPermission {
     Visitor,
@@ -14,11 +16,11 @@ enum class PlayerPermission {
 
     companion object : ProtoCodec<PlayerPermission> {
         override fun serialize(value: PlayerPermission, stream: Sink) {
-            Proto.Byte.serialize(value.ordinal.toByte(), stream)
+            ProtoVAR.Int.serialize(value.ordinal, stream)
         }
 
         override fun deserialize(stream: Source): PlayerPermission {
-            return entries[Proto.Byte.deserialize(stream).toInt()]
+            return entries[ProtoVAR.Int.deserialize(stream)]
         }
     }
 }
